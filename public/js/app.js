b3502b466f05ae020bf7e7a01278bfe6634799ba
Patrick Chong <pcjh@umich.edu>
2018-10-09 13:09:34 -0400

    First node app deploy

---
+++ public/js/app.js
@@ -0,0 +1,104 @@
+function downloadZip() {
+  if (IMAGE_COUNTER > 0) {
+    var zip = new JSZip()
+
+    var links = document.querySelectorAll('.img_link')
+    for (var i = 0; i < links.length; ++i) {
+      // Add a file to the directory, in this case an image with data URI as contents
+      zip.file(links[i].download, links[i].href.split('base64,')[1], {
+        base64: true
+      })
+    }
+
+    // Generate the zip file asynchronously
+    zip
+      .generateAsync({
+        type: 'blob'
+      })
+      .then(function(content) {
+        // Force down of the Zip file
+        saveAs(content, 'images.zip')
+      })
+  }
+}
+
+function newFiles(element) {
+  if (element.files.length > 20) {
+    alert('Too many files uploaded! (Max 20)')
+    return
+  }
+
+  document.querySelector('#img_placeholder').style.display = 'none'
+  document.querySelector('#download-options').style.display = 'none'
+  showLoading()
+  showStatus('Uploading Images...')
+
+  var app = document.getElementById('app')
+  UPLOADED += element.files.length
+  for (var i = 0; i < element.files.length; ++i) {
+    readFileAndProcess(element.files[i])
+  }
+}
+
+function readFileAndProcess(file) {
+  var formData = new FormData()
+  var request = new XMLHttpRequest()
+  formData.set('file', file)
+
+  request.onreadystatechange = function() {
+    //Call a function when the state changes.
+    if (request.readyState == 4 && request.status == 200) {
+      addImgToPage(request.responseText, 'ok')
+    }
+  }
+  //Send the proper header information along with the request
+  request.open('POST', '/', true)
+  request.setRequestHeader('Cache-Control', 'no-cache')
+  request.send(formData)
+}
+
+function addImgToPage(image, fileName) {
+  var squared_file =
+    fileName.split('.')[0] + '_squared.' + fileName.split('.')[1]
+
+  var img_wrapper = document.createElement('div')
+  img_wrapper.classList.add('img_wrapper')
+
+  var img_data = document.createElement('img')
+  img_data.classList.add('img_data')
+  img_data.setAttribute('src', image)
+
+  var img_link = document.createElement('a')
+  img_link.classList.add('img_link')
+  img_link.setAttribute('href', image)
+  img_link.setAttribute('download', squared_file)
+
+  var removeBtn = document.createElement('span')
+  removeBtn.classList.add('remove')
+  removeBtn.innerHTML = '&times;'
+  removeBtn.addEventListener('click', function() {
+    --IMAGE_COUNTER
+    --UPLOADED
+    this.parentNode.parentNode.parentNode.removeChild(
+      this.parentNode.parentNode
+    )
+    if (document.querySelectorAll('.img_link').length == 0) {
+      document.querySelector('#download-options').style.display = 'none'
+      document.getElementById('img_placeholder').style.display = 'grid'
+    }
+  })
+
+  img_link.appendChild(img_data)
+  var div = document.createElement('div')
+  div.appendChild(img_link)
+  div.appendChild(removeBtn)
+  img_wrapper.appendChild(div)
+  app.appendChild(img_wrapper)
+
+  ++IMAGE_COUNTER
+  if (IMAGE_COUNTER === UPLOADED) {
+    document.querySelector('#download-options').style.display = 'block'
+    hideLoading()
+    hideStatus()
+  }
+}
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+// const bodyParser = require('body-parser');
+const app = express()
+const jimp = require('jimp')
+// const { parse } = require('querystring');
+var multer = require('multer')
+var upload = multer({ dest: 'uploads/' })
+var fs = require('fs')
+var path = require('path')
+
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res) {
+  res.render('index')
+})
+
+app.post('/', upload.single('file'), function(req, res) {
+
+  jimp.read(req.file.path).then(function (image) {
+    if (image.bitmap.width > image.bitmap.height) {
+      image.contain(image.bitmap.width, image.bitmap.width).quality(100)
+        .background(0xFFFFFFFF)
+        .getBase64(jimp.AUTO, function (err, src) {
+          if (err) throw err;
+          res.send(src)
+        })
+
+    } else {
+      image.contain(image.bitmap.height, image.bitmap.height).quality(100)
+        .background(0xFFFFFFFF)
+        .getBase64(jimp.AUTO, function (err, src) {
+          if (err) throw err;
+          res.send(src)
+        })
+    }
+  })
+  
+})
+app.listen(8000, function() {
+  console.log('Example app listening on port 3000!')
+})
